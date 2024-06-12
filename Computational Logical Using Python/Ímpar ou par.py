308f763c6a69874dd9b3e3f904d69e679049312a
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-12 11:45:49 -0300

    Create Ímpar ou par.py

---
+++ Computational Logical Using Python/Ímpar ou par.py
@@ -0,0 +1,17 @@
+numero = int(input('Digite um número qualquer: '))
+
+conta = numero % 2
+
+if conta == 0:
+    print('Esse número é par!')
+else:
+    print ('Esse número é ímpar!')
+print ('FIM DO PROGRAMA!')
+
+# Este código em Python tem a função de determinar se um número fornecido pelo usuário é ímpar ou par.
+
+# O usuário entra com um número de sua escolha e o programa calcula o resto da divisão desse número por 2.
+
+# Posteriormente, o programa verifica se o resto da divisão é zero. Caso seja, conclui que o número é par.
+
+# Se o resto da divisão não for zero, o programa informa que o número é ímpar.
