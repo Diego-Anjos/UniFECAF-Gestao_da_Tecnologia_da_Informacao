4365757f70af440469b22f29b5d627c1310533a0
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-14 11:09:31 -0300

    Create exercicio04.py

---
+++ Computational Logical Using Python/exercicio04.py
@@ -0,0 +1,8 @@
+# 4) Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius (baseado na fórmula abaixo):
+# °C = (°F − 32) ÷ 1.8
+
+temperatura = float(input('Informe a temperatura em Celcius: '));
+
+f = (1.8 * temperatura) + 32;
+
+print ('A temperatura em graus Fahrenheit é:', f); 
