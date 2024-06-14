c36dd1d3ffec2bd5d3568bb3709658369eb457d1
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-14 11:12:31 -0300

    Create introdução ao comando if.py

---
+++ Computational Logical Using Python/introdução ao comando if.py
@@ -0,0 +1,10 @@
+# COMANDO IF
+# Sintaxe abaixo:
+# Programa de introdução aos comandos IF e ELSE
+
+idade = int(input('Digite a idade: '))
+
+if idade >= 18:
+
+    print ('Maior de idade!')
+print ('Fim do programa')
