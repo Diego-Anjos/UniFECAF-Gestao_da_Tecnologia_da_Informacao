e9fd47c3142a2672a07d4b8f9c1ba9762fd5018d
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-16 12:55:22 -0300

    Create Estrutura while exemplo1.py

---
+++ Computational Logical Using Python/Estrutura while exemplo1.py
@@ -0,0 +1,9 @@
+# Exemplo de estrutura de repetição.
+# Enquanto a condição contador <= 100 for verdadeira, o bloco de código dentro do loop será executado. 
+# Ou seja, o loop continuará a ser executado enquanto o valor de contador for menor ou igual a 100.
+
+contador = 0
+while contador <= 100:
+    print (contador)
+    contador = contador + 10
+print ('FIM DO PROGRAMA')
