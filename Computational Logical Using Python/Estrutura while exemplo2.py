b9baafaa4be6bbb60e726f55a4e049da50b2ca7b
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-16 12:59:07 -0300

    Create Estrutura while exemplo2.py

---
+++ Computational Logical Using Python/Estrutura while exemplo2.py
@@ -0,0 +1,10 @@
+# Exemplo de estrutura While.
+# A cada iteração do loop, o valor de cont aumenta em mais 25.
+
+cont = 0
+
+while cont < 100:
+    cont = cont + 25
+    print (cont)
+    print ('Contando até 100')
+print ('FIM DO PROGRAMA.')
