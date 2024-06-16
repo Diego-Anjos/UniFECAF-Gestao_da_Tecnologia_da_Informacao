12cfee96f19bf0bc5d29e2e997b6aa013ca0f4e6
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-16 13:06:14 -0300

    Create Estrutura while exemplo4.py

---
+++ Computational Logical Using Python/Estrutura while exemplo4.py
@@ -0,0 +1,9 @@
+# Exemplo de estrutura While que imprime "Good Night" 5 vezes.
+
+contador = 1
+
+while contador <= 5:
+    print ('Good Night!')
+    print (contador)
+    contador = contador + 1
+print ("Finish the program. ")
