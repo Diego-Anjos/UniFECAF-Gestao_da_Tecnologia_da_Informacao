29a4262949b5c49682e190a12c70eb5cec3d5b8c
Diego Anjos <166080180+Diego-Anjos@users.noreply.github.com>
2024-06-16 13:01:51 -0300

    Create Estrutura while exemplo3.py

---
+++ Computational Logical Using Python/Estrutura while exemplo3.py
@@ -0,0 +1,9 @@
+# Cada interação do comando While, o valor de cont diminui em 200.
+
+cont = 1000
+
+while cont > 0:
+    cont = cont - 200
+    print (cont)
+    print ('Contagem regressiva.')
+print ('FIM PROGRAMA.')
